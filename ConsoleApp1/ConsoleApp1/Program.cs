1d81bbe76fae64e192cab9f2c57c9ed0a155442b
adamOboda <adam.oboda1998@gmail.com>
2022-10-25 14:02:00 +0200

    FirstwithVisualStudio

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    abstract class Animal
+    {
+        public abstract void animalSound();
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+
+    }
+
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("Pig says: Oink Oink");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();
+            myPig.animalSound();
+            myPig.sleep();
+
+
+        }
+    }
+}
